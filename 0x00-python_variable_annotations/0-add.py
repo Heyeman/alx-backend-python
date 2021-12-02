377c45b9636f37750e3b4bae01a75051cf8c4d55
heyeman <abdisaheyeman083@gmail.com>
2021-12-02 17:57:57 +0300

    updated the docs

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""Adds two floats together. All your files will be interpreted/compiled on Ubuntu 18.04 LTS using python3 (version 3.7)"""
+"""Adds two floats together.on Ubuntu 18.04 LTS using python3 (version 3.7)"""
 
 
 def add(a: float, b: float) -> float:
