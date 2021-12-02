e9512ffafb515a1a4fcee5c07ef8481313f2773f
heyeman <abdisaheyeman083@gmail.com>
2021-12-02 18:20:23 +0300

    added sum func

---
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""contains some shit"""
+from typing import List
+
+mxd_lst = List[int, float, ...]
+
+
+def sum_mixed_list(mxd_lst: mxd_lst) -> float:
+    """returns sum of the floats contained in the mxd_lst"""
+    return sum(mxd_lst)
